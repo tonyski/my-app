36d68e157a2f4c1a7e7f51d98ab2e4cba14e94b2
tonyski <ztonyski@gmail.com>
2025-03-23 15:44:23 +0800

    更新缓存

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import "jsr:@std/dotenv/load";
 import { Hono } from "hono";
 import { getDb } from "./plugins/db.ts";
+import { cache } from "hono/cache";
 
 const app = new Hono();
+
+app.get(
+  "*",
+  cache({ cacheName: "my-app", cacheControl: "max-age=3600", wait: true })
+);
 
 app.get("/", async (c) => {
   const db = await getDb(c);
